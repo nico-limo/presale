9f33e6d135f17b09b9f1035b627b288e75a3934b
nico-limo <nicolas.limonoff@gmail.com>
2023-08-31 15:32:01 -0300

    added layout height

--- components/Layout/Navbar.tsx
+++ components/Layout/Navbar.tsx
@@ -1,14 +1,12 @@
-import { Header, Container, rem, Text } from "@mantine/core"
+import { Header, Container, Text } from "@mantine/core"
 import Walletconnect from "../Walletconnect"
-import { useStyles } from "@/styles/mantineStyles"
-
-const HEADER_HEIGHT = rem(60)
+import { LAYOUT_HEIGHT, useStyles } from "@/styles/mantineStyles"
 
 const Navbar = () => {
   const { classes } = useStyles()
 
   return (
-    <Header height={HEADER_HEIGHT} className={classes.root}>
+    <Header height={LAYOUT_HEIGHT} className={classes.root}>
       <Container className={classes.header}>
         <Text fz="xl" fw={700}>
           PRESALE APP
