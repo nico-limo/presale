93c5d6221426cd457357d0c9ff3e58ecc248abd8
nico-limo <nicolas.limonoff@gmail.com>
2023-09-08 14:44:05 -0300

    added connectkit

--- components/Walletconnect/index.tsx
+++ components/Walletconnect/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@mantine/core"
+import { ConnectKitButton } from "connectkit"
 
 import { useAccount, useConnect } from "wagmi"
 import { disconnect } from "wagmi/actions"
@@ -15,18 +15,7 @@ const Walletconnect = () => {
     }
   }
 
-  if (address)
-    return (
-      <Button onClick={() => onConnect(false)} radius="lg" color="grape">
-        {`${address.slice(0, 4)}...${address.slice(-4)}`}
-      </Button>
-    )
-
-  return (
-    <Button onClick={() => onConnect(true)} radius="lg">
-      Connect Wallet
-    </Button>
-  )
+  return <ConnectKitButton />
 }
 
 export default Walletconnect
